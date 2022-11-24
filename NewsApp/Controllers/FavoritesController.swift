fba6fac0d9cc09156bbca4e2a66ec5103a072f2e
Michael Kolkov <55915846+mireabot@users.noreply.github.com>
2022-11-24 15:49:19 -0500

    Added favs controller

---
+++ NewsApp/Controllers/FavoritesController.swift
@@ -0,0 +1,25 @@
+//
+//  FavoritesController.swift
+//  NewsApp
+//
+//  Created by Mikhail Kolkov on 11/24/22.
+//
+
+import UIKit
+
+class FavoritesController: UIViewController {
+    //MARK: - Properties
+    
+    //MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        navigationItem.title = "Favorites"
+    }
+    
+    //MARK: - Helpers
+    
+    //MARK: - Selectors
+}
+
